5087399c506aed8cee341820cd3fff6018de1c19
tokazato <tokazato1992>
2019-08-20 19:44:35 +0400

    day1
    day1

--- assets/js/intSlider.js
+++ assets/js/intSlider.js
@@ -1,5 +1,9 @@
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
+    navigation: {
+      nextEl: '.mainSliderRight',
+      prevEl: '.mainSliderLeft',
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $('.openHeaderDropDown').hover(function(){
-               $('.dropDownContent').slideDown('slow').css('display', 'flex');
-           }, function(){
-            $('.dropDownContent').slideUp();
-           });
+         $('.dropDownContent').slideDown('slow').css('display', 'flex');
+        }, function(){
+        $('.dropDownContent').slideUp();
+    });
+    // $('.headerLanguageLi').hover(function(){
+    //     $('.downLanguage').slideDown('slow').css('display', 'flex');
+    //    }, function(){
+    //    $('.downLanguage').slideUp();
+//    });
 })
