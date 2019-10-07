2f1dac6c123734f8b35e58cafc491405f33fd507
tokazato <tokazato1992>
2019-10-07 09:34:07 +0400

    add js
    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,35 @@ $(document).ready(function(){
         $('.attention').toggle();
     });
 })
+
+var startToCalculate = document.getElementById('startToCalculate')
+var getMoney = document.getElementById('getMoney');
+var annualPercent = document.getElementById('annualPercent');
+var monthly = document.getElementById('monthly');
+// function calculator(){
+//     if(getMoney.value < 5000){
+//         annualPercent.value = 24;
+//         monthly.value = 2;
+//     }
+//     if(getMoney.value > 5000){
+//         annualPercent.value = 18;
+//         monthly.value = 1.5;
+//     }
+//     if(getMoney.value > 10000){
+//         annualPercent.value = 12;
+//         monthly.value = 1;
+//     }
+// }
+// startToCalculate.addEventListener('click', calculator );
+
+monthly.addEventListener('keyup', function(){
+    var savePercentMonthly =  monthly.value * 12;
+    var saveToFixedNumberMonthly = savePercentMonthly.toFixed(2)
+    annualPercent.value = saveToFixedNumberMonthly;
+})
+annualPercent.addEventListener('keyup', function(){
+    var savePercent =  annualPercent.value / 12;
+    var saveToFixedNumber = savePercent.toFixed(2)
+    monthly.value = saveToFixedNumber;
+})
+ 
