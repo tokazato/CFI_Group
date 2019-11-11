df80f4e298d7ee59393f9dbd58ad44d711a98743
tokazato <tokazato1992>
2019-11-11 10:44:18 +0400

    add function
    add function

--- assets/js/int.easy-calc.js
+++ assets/js/int.easy-calc.js
@@ -3,5 +3,6 @@ var swiper = new Swiper('.easy-swiper-container', {
       nextEl: '.easy-calc-swiper-go-right',
       prevEl: '.easy-calc-swiper-go-left',
     },
+    spaceBetween: 10,
     slidesPerView: '1.5',
   });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
 // ----------------  start jquery
+$(document).ready(function(){
+
 
 
 // -------------- navigation drop down in menu
@@ -102,56 +103,245 @@ $(document).ready(function(){
         $('.popup').hide();
     })
 
+// ------------------------- check contact page form
 
-// --------------- check contact page form
-    $('.contact-send-button').on('click', function(){
-        var cntInput = $('.contact-input');
-        var cntIcon1 = $('.contact-icon1');
-        var cntIcon2= $('.contact-icon2');
+    var emailIcon = $('.contact-icon2');
+    var userIcon = $('.contact-icon1');
+    var emailInput =  $('.contact-email');
+    var nameInput = $('.contact-first-name');
+    var cntTextarea = $('.contact-textarea ');
 
-        for(let i = 0; i < cntInput.length; i++ ){
-            if( cntInput.eq(i).val().length < 2 ){
-                // cntInput.eq(i).css('color', 'red');
-                cntInput.eq(i).css('border-color', '#E2574C');
-                cntIcon1.eq(i).css('background', '#E2574C');
-                cntIcon2.eq(i).css('background', '#E2574C');
-                
+    $('.contact-send-button').on('click', function(event){
+
+        // ------ check email on click
+        var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+            if (testEmail.test( emailInput.val())) {
+            emailInput.removeClass('form-error');
+            emailIcon.css({'background': '#57697B'});
             } else {
-                // cntInput.eq(i).css('color', '#102943');
-                cntInput.eq(i).css('border-color', '#102943');
-                cntIcon1.eq(i).css('background', '#102943');
-                cntIcon2.eq(i).css('background', '#102943');
+            emailInput.addClass('form-error');
+            emailIcon.css({'background': '#E2574C'});
             }
-        }  
+
+        // ----- check name on click
+            if(nameInput.val().length < 2){
+                userIcon.css('background', '#E2574C');
+                nameInput.addClass('form-error');
+            } else {
+                userIcon.css('background', '#57697B');
+               nameInput.removeClass('form-error');
+            }
+
+        // ------- check textarea on click
+            if(cntTextarea.val().length < 10){
+                cntTextarea.addClass('form-error');
+            } else {
+                cntTextarea.removeClass('form-error');
+            }
+
+        // ------- stop function if intup field is wrong
+            if( $('input').hasClass('form-error') ||  $('textarea').hasClass('form-error') ){
+                event.preventDefault();
+            }
     })
-    $('.contact-email').keyup(function(){
+   
+    // ------ check email on keyup
+    emailInput.keyup(function(){
         if($(this).is('input[type="email"]')){
             var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
             if (testEmail.test($(this).val())) {
-            $(this).css({'border-color': '#102943'});
-            $('.contact-icon2').css('background', '#102943');
+            $(this).removeClass('form-error');
+            emailIcon.css({'background': '#57697B'});
             }else{
-            $(this).css({'border-color': '#E2574C'});
-            $('.contact-icon2').css('background', '#E2574C');
+            $(this).addClass('form-error');
+            emailIcon.css({'background': '#E2574C'});
             }
         }
     })
-    $('.contact-first-name').keyup(function(){
+
+    // ------ check name on keyup
+    nameInput.keyup(function(){
         if($(this).val().length < 2){
-            $(this).css('border-color', '#E2574C');
-            $('.contact-icon1').css('background', '#E2574C');
+            $(this).addClass('form-error');
+            userIcon.css('background', '#E2574C');
         }else{
-            $(this).css('border-color', '#102943');
-            $('.contact-icon1').css('background', '#102943');
+            $(this).removeClass('form-error');
+            userIcon.css('background', '#57697B');
             }
     })
+
+    // ------ check textarea on keyup
+    cntTextarea.keyup(function(){
+        if($(this).val().length < 10){
+            $(this).addClass('form-error');
+        }else{
+            $(this).removeClass('form-error');
+            }
+    })
+
+    
+// ---------------- calculator monthly limit
+    $('.monthly-loan').keyup(function(){
+        if( $(this).val() > 240 ){
+            $(this).val('240')
+        } 
+        if( $(this).val() < 0 ){
+            $(this).val('0')
+        }
+        if( $(this).val().length > 3 ){
+            $(this).val('240');
+        }
+    })
+
+// --------------  type only text in input
+    // $('.form-only-text').keypress(function(){
+    //     return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)
+    // })
+
+
+
+
+
+// --------------- check input value for number, number lenght, text.
+    function setInputFilter(textbox, inputFilter) {
+        ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+          textbox.addEventListener(event, function() {
+            if (inputFilter(this.value)) {
+              this.oldValue = this.value;
+              this.oldSelectionStart = this.selectionStart;
+              this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+              this.value = this.oldValue;
+              this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+          });
+        });
+      }
+
+
+    // ------------ check input id value that user type only number in Form 
+    var idNumber = $('.form-id-number');
+    for(let i = 0; i < idNumber.length; i++){
+        setInputFilter(idNumber[i], function(value) {
+            return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 310000000000); });
+    }
+
+    // ---------------  check input id value that max length to be 11 number
+    var max_chars = 10;
+    $('.form-id-number').keydown( function(e){
+        if ($(this).val().length >= max_chars) { 
+            $(this).val($(this).val().substr(0, max_chars));
+        }
+    });
+    // $('#input').keyup( function(e){
+    //     if ($(this).val().length >= max_chars) { 
+    //         $(this).val($(this).val().substr(0, max_chars));
+    //     }
+    // });
+
+
+    // ----------- check form input that to be only text
+    var onlyText = document.getElementsByClassName("form-only-text");
+    for(let i = 0; i < onlyText.length; i++ ){
+        setInputFilter(onlyText[i], function(value) {
+        return /^[a-z\u00c0-\u024f]*$/i.test(value); });
+    }
+
+    //   setInputFilter(document.getElementsByClassName("form-only-text")[0], function(value) {
+    //     return /^-?\d*$/.test(value); });
+
+    //   setInputFilter(document.getElementsByClassName("form-only-text")[0], function(value) {
+    //     return /^-?\d*[.,]?\d*$/.test(value); });
+
+    //   setInputFilter(document.getElementsByClassName("form-only-text")[0], function(value) {
+    //     return /^-?\d*[.,]?\d{0,2}$/.test(value); });
+
+    //   setInputFilter(document.getElementsByClassName("form-only-text")[0], function(value) {
+    //     return /^[a-z]*$/i.test(value); });
+
+    //   setInputFilter(document.getElementsByClassName("form-only-text")[0], function(value) {
+    //     return /^[0-9a-f]*$/i.test(value); });
+
+
+    // setInputFilter(document.getElementsByClassName("form-only-text")[0], function(value) {
+    //     return /^[a-z\u00c0-\u024f]*$/i.test(value); });
+
+
+
+// --------------- check loans form on click
+    $('.send-form').on('click', function(event){
+
+        $('.fill-loan-main-box :input').each(function(){
+            var input = $(this);
+
+            // ------- check input value length
+            if( input.val().length < 2){
+                $(this).addClass('form-error');
+            } else {
+                $(this).removeClass('form-error');
+            }
+
+            // ------- check id Number value
+            for(let i = 0; i < idNumber.length; i++ ){
+                if( idNumber.eq(i).val().length != 11 ){
+                    idNumber.eq(i).addClass('form-error')
+                } 
+                else {
+                    idNumber.eq(i).removeClass('form-error')
+                }
+            }
+
+            // --------- add error class radion buttons only we choose
+            if( $('.fill-form-input-up-box-right-side').css('display') == 'none'  ){
+                $('.fill-form-input-up-box-right-side input').removeClass('form-error');
+            } 
+            if( $('.fill-form-radio-botton-left-side-box ').css('display') == 'none'  ){
+                $('.fill-form-radio-botton-left-side-box  input').removeClass('form-error');
+            } 
+
+            // ---------- stop function if error is active
+            if( $('input').hasClass('form-error') ){
+                event.preventDefault()
+            }
+
+        })
+    })
+
+
+// ------------------- check loans form on key up
+
+    // ------- check input length
+    $('.fill-loan-main-box :input').each(function(){
+        var input = $(this);
+        input.keyup(function(){
+            if( input.val().length < 2){
+                $(this).addClass('form-error');
+            } else {
+                $(this).removeClass('form-error');
+            }
+        })
+    })
+
+    // --------- check id number
+    $(idNumber).keyup(function(){
+            if( $(this).val().length != 11 ){
+                $(this).addClass('form-error')
+            } 
+            else {
+                $(this).removeClass('form-error')
+            }
+    })
+
+    // check input type number that users can't type "e"
+    $('.form-phone-number').keydown(function(){
+        return event.keyCode !== 69;
+    })
+
     
 
-
-
     
-// -------------- end jquery     
-})
+ 
+}) // -------------- end jquery    
 
 
 // ---------- calculator 
@@ -181,13 +371,21 @@ var monthly = document.getElementById('monthly');
 
 
 // ------------ calculator: monthly and annual counter
-monthly.addEventListener('keyup', function(){
-    var savePercentMonthly =  monthly.value * 12;
-    var saveToFixedNumberMonthly = savePercentMonthly.toFixed(2)
-    annualPercent.value = saveToFixedNumberMonthly;
-})
-annualPercent.addEventListener('keyup', function(){
-    var savePercent =  annualPercent.value / 12;
-    var saveToFixedNumber = savePercent.toFixed(2)
-    monthly.value = saveToFixedNumber;
-})
+
+try {
+    monthly.addEventListener('keyup', function(){
+        var savePercentMonthly =  monthly.value * 12;
+        var saveToFixedNumberMonthly = savePercentMonthly.toFixed(2)
+        annualPercent.value = saveToFixedNumberMonthly;
+    })
+    annualPercent.addEventListener('keyup', function(){
+        var savePercent =  annualPercent.value / 12;
+        var saveToFixedNumber = savePercent.toFixed(2)
+        monthly.value = saveToFixedNumber;
+    })  
+} catch (error) {
+    
+}
+
+
+
